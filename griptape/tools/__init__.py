d7c8d1f19e3be83816f1d99fb8561a8d9391128b
Vasily Vasinov <des.elyon@gmail.com>
2023-04-12 15:18:36 -0600

    Fix imports

--- griptape/tools/__init__.py
+++ griptape/tools/__init__.py
@@ -1,9 +1,11 @@
 from .calculator.tool import Calculator
 from .web_search.tool import WebSearch
 from .web_scraper.tool import WebScraper
+from .sql_client.tool import SqlClient
 
 __all__ = [
     "Calculator",
     "WebSearch",
-    "WebScraper"
+    "WebScraper",
+    "SqlClient"
 ]
