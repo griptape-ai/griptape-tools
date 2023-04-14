30b55ee57e88edad70bb90f93d3720264f87b7f6
Vasily Vasinov <des.elyon@gmail.com>
2023-04-14 14:00:31 -0600

    Fix EmailClient.send

--- griptape/tools/email_client/tool.py
+++ griptape/tools/email_client/tool.py
@@ -1,7 +1,7 @@
-import json
+import ast
+import logging
 import smtplib
-import ssl
-from email.message import EmailMessage
+from email.mime.text import MIMEText
 from typing import Optional
 from attr import define, field
 from griptape.core import BaseTool, action
@@ -38,30 +38,32 @@ class EmailClient(BaseTool):
     })
     def send(self, value: bytes) -> str:
         server: Optional[smtplib.SMTP] = None
-        params = json.loads(value.decode())
+        params = ast.literal_eval(value.decode())
+        host = self.env_value("SMTP_HOST")
+        port = int(self.env_value("SMTP_PORT"))
         to_email = params["to"]
+        from_email = self.env_value("FROM_EMAIL")
         subject = params["subject"]
-        body = params["body"]
-        msg = EmailMessage()
-
-        msg['Subject'] = subject
-        msg['From'] = self.env_value("FROM_EMAIL")
-        msg['To'] = to_email
+        password = self.env_value("SMTP_PASSWORD")
+        msg = MIMEText(params["body"])
 
         try:
-            server = smtplib.SMTP(self.env_value("SMTP_HOST"), int(self.env_value("SMTP_PORT")))
-
             if self.env_value("SMTP_USE_SSL") == "True":
-                server.starttls(context=ssl.create_default_context())
+                server = smtplib.SMTP_SSL(host, port)
+            else:
+                server = smtplib.SMTP(host, port)
 
-            if self.env_value("SMTP_PASSWORD") != "":
-                server.login(self.env_value("FROM_EMAIL"), self.env_value("SMTP_PASSWORD"))
+            msg["Subject"] = subject
+            msg["From"] = from_email
+            msg["To"] = to_email
 
-            msg.set_content(body)
-            server.send_message(msg)
+            server.login(from_email, password)
+            server.sendmail(from_email, [to_email], msg.as_string())
 
             return "email was successfully sent"
         except Exception as e:
+            logging.error(e)
+
             return f"error sending email: {e}"
         finally:
             try:
