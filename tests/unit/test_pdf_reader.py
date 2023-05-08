200159e1b1cc51a58394ddb931c88d17202d30be
Alex <codeetc@pm.me>
2023-05-08 17:15:40 +0100

    feat: a PDF reader tool

---
+++ tests/unit/test_pdf_reader.py
@@ -0,0 +1,32 @@
+import random
+
+import pytest
+from griptape.artifacts import TextArtifact, ErrorArtifact
+
+from tests.utils import install_requirements
+
+
+@pytest.mark.usefixtures("install_requirements")
+class TestPdfReader:
+    @pytest.fixture(scope="class")
+    def install_requirements(request):
+        install_requirements("pdf_reader")
+
+    @pytest.fixture
+    def pdf_reader(self):
+        from griptape.tools import PdfReader
+        return PdfReader()
+
+    def test_read_pdf(self, pdf_reader):
+        artifact = pdf_reader.get_content("./bitcoin.pdf")
+        assert isinstance(artifact, TextArtifact)
+
+        response = artifact.value
+
+        assert response.startswith("Bitcoin: A Peer-to-Peer Electronic Cash System")
+
+    def test_read_pdf_fails(self, pdf_reader):
+        n = random.randint(0, 100)
+        artifact = pdf_reader.get_content(f"./this-will-never-exist-{n}.pdf")
+        assert isinstance(artifact, ErrorArtifact)
+        assert artifact.exception.args[0]
