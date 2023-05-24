49b3d38a92d0d1483de5afd8eb2340141071743e
Collin Dutter <collindutter@gmail.com>
2023-05-23 17:11:20 -0700

    Fix description

--- griptape/tools/rest_api_client/tool.py
+++ griptape/tools/rest_api_client/tool.py
@@ -48,7 +48,7 @@ class RestApi(BaseTool):
         config={
             "description": dedent(
                 """
-                This tool can be used to make a patch request to the rest api url: "{{base_url}}{% if path %}/{{path}}{% endif %}".
+                This tool can be used to make a put request to the rest api url: "{{base_url}}{% if path %}/{{path}}{% endif %}".
                 This rest api does the following: "{{description}}".
                 {% if request_body_schema %}The request body must follow this JSON schema: {{request_body_schema}}{% endif %}.
                 {% if response_body_schema %}The response body must follow this JSON schema: {{response_body_schema}}{% endif %}.
@@ -121,7 +121,7 @@ class RestApi(BaseTool):
         config={
             "description": dedent(
                 """
-                This tool can be used to make a patch request to the rest api url: "{{base_url}}{% if path %}/{{path}}{% endif %}".
+                This tool can be used to make a post request to the rest api url: "{{base_url}}{% if path %}/{{path}}{% endif %}".
                 This rest api does the following: "{{description}}".
                 {% if request_body_schema %}The request body must follow this JSON schema: {{request_body_schema}}{% endif %}.
                 {% if response_body_schema %}The response body must follow this JSON schema: {{response_body_schema}}{% endif %}.
@@ -151,7 +151,7 @@ class RestApi(BaseTool):
         config={
             "description": dedent(
                 """
-                This tool can be used to make a patch request to the rest api url: "{{base_url}}{% if path %}/{{path}}{% endif %}".
+                This tool can be used to make a get request to the rest api url: "{{base_url}}{% if path %}/{{path}}{% endif %}".
                 This rest api does the following: "{{description}}".
                 {% if request_path_parameters %}The request path parameters must follow this JSON schema: {{request_path_params_schema}}{% endif %}.
                 {% if request_query_parameters %}The request query parameters must follow this JSON schema: {{request_path_params_schema}}{% endif %}.
@@ -200,7 +200,7 @@ class RestApi(BaseTool):
         config={
             "description": dedent(
                 """
-                This tool can be used to make a patch request to the rest api url: "{{base_url}}{% if path %}/{{path}}{% endif %}".
+                This tool can be used to make a delete request to the rest api url: "{{base_url}}{% if path %}/{{path}}{% endif %}".
                 This rest api does the following: "{{description}}".
                 {% if request_path_parameters %}The request path parameters must follow this JSON schema: {{request_path_params_schema}}{% endif %}.
                 {% if request_query_parameters %}The request query parameters must follow this JSON schema: {{request_path_params_schema}}{% endif %}.
