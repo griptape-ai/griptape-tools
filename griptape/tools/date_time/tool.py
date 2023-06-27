0dc761c7aed2021c7eea114fc5cce598a2b0fe4d
Devin Jones <devintj@uci.edu>
2023-06-26 19:34:53 -0700

    Modified datetime tool & testfile to meet changes suggested by Vasily

--- griptape/tools/date_time/tool.py
+++ griptape/tools/date_time/tool.py
@@ -2,13 +2,11 @@ from datetime import datetime
 from griptape.artifacts import BaseArtifact, ErrorArtifact, TextArtifact
 from griptape.core import BaseTool
 from griptape.core.decorators import activity
-from schema import Schema
 
 
 class DateTime(BaseTool):
     @activity(config={
-        "description": "Returns the current date and time",
-        "schema": Schema({},ignore_extra_keys = True)
+        "description": "Returns the current date and time"
     })
     def get_current_datetime(self, params: dict) -> BaseArtifact:
         try:
--- tests/unit/test_date_time.py
+++ tests/unit/test_date_time.py
@@ -5,4 +5,5 @@ from datetime import datetime
 class TestDateTime:
     def test_get_current_datetime(self):
         result = DateTime().get_current_datetime({})
-        assert datetime.strptime(result.value, "%Y-%m-%d %H:%M:%S.%f").date() == datetime.now().date()
+        time_delta = datetime.strptime(result.value, "%Y-%m-%d %H:%M:%S.%f") - datetime.now()
+        assert abs(time_delta.total_seconds()) <= 1000
