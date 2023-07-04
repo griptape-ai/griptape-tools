304b44a3fa38f9d6c436a75c3a8b01346d64981e
Kyle Roche <kyleroche@users.noreply.github.com>
2023-07-03 17:26:26 -0700

    get user policy

--- griptape/tools/aws_iam_client/tool.py
+++ griptape/tools/aws_iam_client/tool.py
@@ -6,6 +6,7 @@ from griptape.artifacts import TextArtifact, ErrorArtifact
 from griptape.core.decorators import activity
 from griptape.tools import BaseAwsClient
 
+
 @define
 class AwsIamClient(BaseAwsClient):
     session: boto3.session = field(kw_only=True)
@@ -15,21 +16,62 @@ class AwsIamClient(BaseAwsClient):
     )
 
     @activity(config={
-        "description":"Can be used to list AWS MFA Devices"
+        "description": "Can be use to get a policy for an IAM user.",
+        "schema": Schema({
+            Literal(
+                "user_name",
+                description="Username of the AWS IAM user."
+            ): str,
+            Literal(
+                "policy_name",
+                description="PolicyName of the AWS IAM Policy embedded in the specified IAM user."
+            ): str
+        })
+    })
+    def get_user_policy(self, params: dict) -> [TextArtifact] | ErrorArtifact:
+        try:
+            policy = self.iam_client.get_user_policy(
+                UserName=params["values"]["user_name"],
+                PolicyName=params["values"]["policy_name"]
+            )
+            return TextArtifact(policy["PolicyDocument"])
+        except Exception as e:
+            return ErrorArtifact(f"error returning policy document: {e}")
+
+    @activity(config={
+        "description": "Can be used to list AWS MFA Devices"
     })
     def list_mfa_devices(self, params: dict) -> list[TextArtifact] | ErrorArtifact:
         try:
             devices = self.iam_client.list_mfa_devices()
             return [TextArtifact(str(d)) for d in devices["MFADevices"]]
         except Exception as e:
-            return ErrorArtifact(f"error listing mfa devices")
+            return ErrorArtifact(f"error listing mfa devices: {e}")
 
     @activity(config={
-        "description":"Can be used to list AWS IAM users."
+        "description": "Cna be used to list policies for a given IAM user.",
+        "schema": Schema({
+            Literal(
+                "user_name",
+                description="Username of the AWS IAM user for which to list policies."
+            ): str
+        })
+    })
+    def list_user_policies(self, params: dict) -> list[TextArtifact] | ErrorArtifact:
+        try:
+            policies = self.iam_client.list_user_policies(
+                UserName=params["values"]["user_name"]
+            )
+            return [TextArtifact(str(p)) for p in policies["PolicyNames"]]
+        except Exception as e:
+            return ErrorArtifact(f"error listing iam user policies: {e}")
+
+    @activity(config={
+        "description": "Can be used to list AWS IAM users."
     })
     def list_users(self, params: dict) -> list[TextArtifact] | ErrorArtifact:
         try:
             users = self.iam_client.list_users()
             return [TextArtifact(str(u)) for u in users["Users"]]
         except Exception as e:
-            return ErrorArtifact(f"error listing s3 users")
+            return ErrorArtifact(f"error listing s3 users: {e}")
--- tests/unit/test_aws_iam.py
+++ tests/unit/test_aws_iam.py
@@ -3,12 +3,29 @@ import boto3
 
 
 class TestAwsIamClient:
-    def test_list_iam_user(self):
-        assert "error listing s3 users" in AwsIamClient(
+    def test_get_user_policy(self):
+        value = {
+            "user_name": "test_user",
+            "policy_name": "test_policy"
+        }
+        assert "error returning policy document" in AwsIamClient(
             session=boto3.Session()
-        ).list_users({}).value
+        ).get_user_policy({"values": value}).value
 
     def test_list_mfa_devices(self):
         assert "error listing mfa devices" in AwsIamClient(
             session=boto3.Session()
         ).list_mfa_devices({}).value
+
+    def test_list_user_policies(self):
+        value = {
+            "user_name": "test_user"
+        }
+        assert "error listing iam user policies" in AwsIamClient(
+            session=boto3.Session()
+        ).list_user_policies({"values": value}).value
+
+    def test_list_users(self):
+        assert "error listing s3 users" in AwsIamClient(
+            session=boto3.Session()
+        ).list_users({}).value
