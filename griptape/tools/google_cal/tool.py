e2058b705aa07c0659d23276bc23521a621a90ab
Kyle Roche <kyleroche@users.noreply.github.com>
2023-07-11 15:08:16 -0700

    adding calendar

---
+++ griptape/tools/google_cal/tool.py
@@ -0,0 +1,43 @@
+import logging
+import datetime
+from schema import Schema, Literal
+from attr import define
+from griptape.artifacts import TextArtifact, ErrorArtifact
+from griptape.core.decorators import activity
+from griptape.tools import BaseGoogleClient
+
+
+@define
+class GoogleCalClient(BaseGoogleClient):
+
+    @activity(config={
+        "description": "Can be used to get upcoming events from a google calendar",
+        "schema": Schema({
+            Literal(
+                "calendar_id",
+                description="id of the google calendar such as 'primary'"
+            ): str
+        })
+    })
+    def get_upcoming_events(self, params: dict) -> list[TextArtifact] | ErrorArtifact:
+        from google.oauth2 import service_account
+        from googleapiclient.discovery import build
+
+        values = params["values"]
+        SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+        try:
+            credentials = service_account.Credentials.from_service_account_info(
+                self.service_account_credentials, scopes=SCOPES
+            )
+            service = build('calendar', 'v3', credentials=credentials)
+            now = datetime.datetime.utcnow().isoformat() + 'Z'
+
+            events_result = service.events().list(
+                calendarId='primary', timeMin=now,
+                maxResults=10, singleEvents=True,
+                orderBy='startTime').execute()
+            events = events_result.get('items', [])
+        except Exception as e:
+            logging.error(e)
+            return ErrorArtifact(f"error retrieving calendar events {e}")
