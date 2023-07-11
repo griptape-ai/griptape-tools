e0faab2d83ee0bc0e16e6a927c71532e2a5d41a9
Vasily Vasinov <des.elyon@gmail.com>
2023-07-12 00:33:33 +0300

    Add the Computer tool (#27)

--- griptape/tools/__init__.py
+++ griptape/tools/__init__.py
@@ -8,14 +8,12 @@ from .file_manager.tool import FileManager
 from .knowledge_base_client.tool import KnowledgeBaseClient
 from .date_time.tool import DateTime
 from .text_memory_browser.tool import TextMemoryBrowser
-
-# AWS Tools
 from .base_aws_client import BaseAwsClient
 from .aws_iam_client.tool import AwsIamClient
 from .aws_s3_client.tool import AwsS3Client
+from .computer.tool import Computer
 
 __all__ = [
-    # AWS
     "BaseAwsClient",
     "AwsIamClient",
     "AwsS3Client",
@@ -28,5 +26,6 @@ __all__ = [
     "FileManager",
     "KnowledgeBaseClient",
     "DateTime",
-    "TextMemoryBrowser"
+    "TextMemoryBrowser",
+    "Computer"
 ]
