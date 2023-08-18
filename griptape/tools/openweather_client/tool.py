13d1eb86a2011b5d0c1b4378cf305396b95f9167
Devin Jones <devintj@uci.edu>
2023-08-18 15:57:56 -0700

    Amended Changes

--- griptape/tools/openweather_client/tool.py
+++ griptape/tools/openweather_client/tool.py
@@ -3,6 +3,7 @@ from griptape.core import BaseTool
 from griptape.core.decorators import activity
 from schema import Schema, Literal
 from attr import define, field
+from typing import Optional
 import requests
 import logging
 
@@ -10,38 +11,37 @@ import logging
 @define
 class OpenWeatherClient(BaseTool):
     BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
-    api_key: str = field(kw_only = True)
+    api_key: str = field(kw_only=True)
+    units: Optional[str] = field(default="imperial", kw_only=True)
 
     @activity(
-        config = {
-            "description": "Fetches weather data for a given city using the OpenWeather API. Temperatures are returned in Fahrenheit by default.",
+        config={
+            "description": "Can be used to fetch weather data for a given city using the OpenWeather API. Temperatures are returned in {self.units} by default.",  # Modified description
             "schema": Schema({
                 Literal(
                     "city_name",
-                    description = "Name of the city to fetch weather data for."
+                    description="Name of the city to fetch weather data for."
                 ): str
             }),
         }
     )
-
     def _get_weather_by_city(self, params: dict):
         city_name = params["values"].get("city_name")
 
         request_params = {
             'q': city_name,
             'appid': self.api_key,
-            'units': 'imperial'
+            'units': self.units
         }
 
         try:
-            response = requests.get(self.BASE_URL, params = request_params)
-            if response.status_code != 200:
+            response = requests.get(self.BASE_URL, params=request_params)
+            if response.status_code == 200:
+                data = response.json()
+                return TextArtifact(str(data))
+            else:
                 logging.error(f"Error fetching weather data. HTTP Status Code: {response.status_code}")
                 return ErrorArtifact("Error fetching weather data from OpenWeather API")
-
-            data = response.json()
-            return TextArtifact(str(data))
-
         except Exception as e:
             logging.error(f"Error fetching weather data: {e}")
             return ErrorArtifact(f"Error fetching weather data: {e}")
--- tests/unit/test_openweather_client.py
+++ tests/unit/test_openweather_client.py
@@ -1,47 +1,41 @@
-import unittest
-from unittest.mock import patch
+import pytest
 from griptape.tools.openweather_client.tool import OpenWeatherClient
 from griptape.artifacts import TextArtifact, ErrorArtifact
 
-class TestOpenWeatherClient(unittest.TestCase):
+@pytest.fixture
+def weather_client():
+    api_key = "YOUR_API_KEY"
+    return OpenWeatherClient(api_key=api_key)
 
-    def setUp(self):
-        self.api_key = "YOUR_API_KEY"
-        self.weather_client = OpenWeatherClient(api_key=self.api_key)
-
-    @patch('griptape.tools.openweather_client.tool.requests.get')
-    def test_get_weather_by_city(self, mock_get):
-        mock_response_data = {
-            "weather": [{
-                "description": "clear sky"
-            }],
-            "main": {
-                "temp": 67.73
-            }
+def test_get_weather_by_city(weather_client, mocker):
+    mock_response_data = {
+        "weather": [{
+            "description": "clear sky"
+        }],
+        "main": {
+            "temp": 67.73
         }
-        mock_get.return_value.json.return_value = mock_response_data
-        mock_get.return_value.status_code = 200
+    }
+    mock_get = mocker.patch('griptape.tools.openweather_client.tool.requests.get')
+    mock_get.return_value.json.return_value = mock_response_data
+    mock_get.return_value.status_code = 200
 
-        city_name = "London"
-        response_artifact = self.weather_client._get_weather_by_city({"values": {"city_name": city_name}})
+    city_name = "London"
+    response_artifact = weather_client._get_weather_by_city({"values": {"city_name": city_name}})
 
-        self.assertIsInstance(response_artifact, TextArtifact)
+    assert isinstance(response_artifact, TextArtifact)
 
-        response_data = eval(response_artifact.to_text())
+    response_data = eval(response_artifact.to_text())
 
-        self.assertEqual(response_data["weather"][0]["description"], "clear sky")
-        self.assertEqual(response_data["main"]["temp"], 67.73)
+    assert response_data["weather"][0]["description"] == "clear sky"
+    assert response_data["main"]["temp"] == 67.73
 
-    @patch('griptape.tools.openweather_client.tool.requests.get')
-    def test_get_weather_by_city_error(self, mock_get):
+def test_get_weather_by_city_error(weather_client, mocker):
+    mock_get = mocker.patch('griptape.tools.openweather_client.tool.requests.get')
+    mock_get.return_value.status_code = 404
 
-        mock_get.return_value.status_code = 404
+    city_name = "NonExistentCity"
+    response_artifact = weather_client._get_weather_by_city({"values": {"city_name": city_name}})
 
-        city_name = "NonExistentCity"
-        response_artifact = self.weather_client._get_weather_by_city({"values": {"city_name": city_name}})
-
-        self.assertIsInstance(response_artifact, ErrorArtifact)
-        self.assertIn("Error fetching weather data", response_artifact.value)
-
-if __name__ == "__main__":
-    unittest.main()
+    assert isinstance(response_artifact, ErrorArtifact)
+    assert "Error fetching weather data" in response_artifact.value
